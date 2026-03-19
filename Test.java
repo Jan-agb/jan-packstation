e0dde69587ec6e2d544e8bb092d829ccfdc0265f
Jan-agb <jan.teipel@agb.nrw.schule>
2026-03-19 14:14:48 +0100

    67

--- Test.java
+++ Test.java
@@ -1,10 +1,13 @@
 public class Test
 {
     Packstation ps1 = new Packstation(12, 23, 7);
-    Paket p1 = new Paket(15, "PAKET#1", 'm');
+    Paket p1 = new Paket(15, "PAKET1", 'M');
     
     public void test()
     {
-        ps1.
+        ps1.anzahlFaecherAusgeben();
+        ps1.freieFaecherAusgeben('M');
+        ps1.paketEinlagern(p1);
+        
     }
 }
